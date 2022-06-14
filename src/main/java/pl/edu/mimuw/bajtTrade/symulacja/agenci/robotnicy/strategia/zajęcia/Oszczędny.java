faa9ad9a221a2a90b4f84306cd771c8f80ec4efc
derivedmate <tosurowiec@gmail.com>
2022-06-15 01:14:38 +0200

    jic;

---
+++ src/main/java/pl/edu/mimuw/bajtTrade/symulacja/agenci/robotnicy/strategia/zajęcia/Oszczędny.java
@@ -0,0 +1,19 @@
+package pl.edu.mimuw.bajtTrade.symulacja.agenci.robotnicy.strategia.zajęcia;
+
+import pl.edu.mimuw.bajtTrade.symulacja.agenci.robotnicy.Robotnik;
+import pl.edu.mimuw.bajtTrade.symulacja.agenci.robotnicy.strategia.StrategiaZajęcia;
+import pl.edu.mimuw.bajtTrade.symulacja.historia.Historia;
+
+public class Oszczędny implements StrategiaZajęcia {
+  private final double limitDiamentów;
+
+  public Oszczędny(double limitDiamentów_) {
+    limitDiamentów = limitDiamentów_;
+  }
+
+  @Override
+  public boolean czySięUczy(Robotnik robotnik, int numerDnia, Historia historia) {
+    return robotnik.
+  }
+  
+}
