125009381d7bd955ccfe3d2b252c1909812219bc
derivedmate <tosurowiec@gmail.com>
2022-06-15 01:50:30 +0200

    nsq;

---
+++ src/main/java/pl/edu/mimuw/bajtTrade/symulacja/giełda/oferty/sprzedaży/OfertaSprzedaży.java
@@ -0,0 +1,20 @@
+package pl.edu.mimuw.bajtTrade.symulacja.giełda.oferty.sprzedaży;
+
+import pl.edu.mimuw.bajtTrade.symulacja.agenci.Agent;
+import pl.edu.mimuw.bajtTrade.symulacja.giełda.oferty.Oferta;
+import pl.edu.mimuw.bajtTrade.symulacja.zadoby.produkty.Produkt;
+
+public class OfertaSprzedaży extends Oferta {
+
+  protected OfertaSprzedaży(Agent wystawiający_, Produkt produkt_) {
+    super(wystawiający_, produkt_);
+    //TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public void wypełnij(Oferta ofertaKomplementacyjna) {
+    // TODO Auto-generated method stub
+    
+  }
+  
+}
